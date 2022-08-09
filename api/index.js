43ed0f7930bc7a0101679a68a1e85b0354658153
Sammy_Ester <sester2@outlook.com>
2022-08-09 13:32:22 -0500

    Utils & the env and some other stuff

--- api/index.js
+++ api/index.js
@@ -1,24 +1,52 @@
-const apiRouter = require('express').Router();
-const usersRouter = require('./users');
-const productsRouter = require('./products');
-const cartsRouter = require('./carts');
+const apiRouter = require("express").Router();
+const usersRouter = require("./users");
+const productsRouter = require("./products");
+const cartsRouter = require("./carts");
+const { getUserById } = require("../db");
+const jwt = require("jsonwebtoken");
+const {JWT_SECRET} = process.env
 
-apiRouter.get('/', (req, res, next) => {
+
+apiRouter.get("/", (req, res, next) => {
   res.send({
-    message: 'API is under construction!',
+    message: "API is under construction!",
   });
 });
 
-apiRouter.get('/health', (req, res, next) => {
+apiRouter.get("/health", (req, res, next) => {
   res.send({
     healthy: true,
   });
 });
 
+apiRouter.use(async (req, res, next) => {
+  const prefix = "Bearer ";
+  const auth = req.header("Authorization");
 
+  if (!auth) {
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: "AuthorizationHeaderError",
+      message: `Authorization token must start with ${prefix}`,
+    });
+  }
+});
 // place your routers here
-apiRouter.use('/users', usersRouter);
-apiRouter.use('/products', productsRouter);
-apiRouter.use('/carts', cartsRouter);
+apiRouter.use("/users", usersRouter);
+apiRouter.use("/products", productsRouter);
+apiRouter.use("/carts", cartsRouter);
 
 module.exports = apiRouter;
--- db/models/user.js
+++ db/models/user.js
@@ -1,12 +1,31 @@
 // grab our db client connection to use with our adapters
-const client = require('../client');
-
+const client = require("../client");
 
 async function getAllUsers() {
   /* this adapter should fetch a list of users from your db */
 }
 
+async function getUserById(userId) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT id, username,email
+      FROM users
+      WHERE id=$1;
+    `,
+      [userId]
+    );
+
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 module.exports = {
   // add your database adapter fns here
   getAllUsers,
+  getUserById,
 };
