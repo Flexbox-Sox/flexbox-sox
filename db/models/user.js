da049099b911945cfbbaf7fddedda4d2fff225c8
Niko <nikoboykin@gmail.com>
2022-08-11 11:23:02 -0400

    Users functions complete

--- db/models/user.js
+++ db/models/user.js
@@ -2,7 +2,17 @@
 const client = require("../client");
 
 async function getAllUsers() {
-  /* this adapter should fetch a list of users from your db */
+  try {
+    const { rows } = await client.query(`
+      SELECT id, email, username, location, isAdmin
+      FROM users;
+    `);
+  
+    return rows;
+  } catch (error) {
+    console.log("Error Getting all Users")
+    throw error;
+  }
 }
 
 async function getUserById(userId) {
@@ -11,7 +21,7 @@ async function getUserById(userId) {
       rows: [user],
     } = await client.query(
       `
-      SELECT id, username,email
+      SELECT id, email, username
       FROM users
       WHERE id=$1;
     `,
@@ -20,12 +30,70 @@ async function getUserById(userId) {
 
     return user;
   } catch (error) {
-    console.log(error);
+    console.log("Error getting User BY ID");
   }
 }
+
+async function getUserByUsername(username) {
+  try {
+    const { rows: [ user ] } = await client.query(`
+      SELECT *
+      FROM users
+      WHERE username=$1
+    `, [ username ]);
+
+    if (!user) {
+      throw {
+        name: "NoUserByThatName",
+        message: "A user with that username does not exist! sorry try again"
+      }
+    }
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateUser(id, fields = {}) {
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${ key }"=$${ index + 1 }`
+  ).join(', ');
+
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const { rows: [ user ] } = await client.query(`
+      UPDATE users
+      SET ${ setString }
+      WHERE id=${ id }
+      RETURNING *;
+    `, Object.values(fields));
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function deleteUser(userId) {
+  await client.query(
+    `
+    DELETE FROM users
+    WHERE id=$1
+    RETURNING *;
+  `,[userId]);
+}
+
+
 
 module.exports = {
   // add your database adapter fns here
   getAllUsers,
   getUserById,
+  getUserByUsername,
+  updateUser,
+  deleteUser
 };
