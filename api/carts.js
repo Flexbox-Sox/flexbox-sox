8c5be66effa5e147b9fb0d43235ee9e33cd939a4
anniemahlberg <amahlberg4@gmail.com>
2022-08-15 14:10:30 -0500

    started cart routes

--- api/carts.js
+++ api/carts.js
@@ -1,11 +1,108 @@
 const express = require("express");
 const cartsRouter = express.Router();
-const { getAllCarts, createCart, updateCart, deleteCart } = require("../db");
+const { getAllCarts, createCart, updateCart, deleteCart, getCartById } = require("../db");
 
-// GET/api/ cartId
+// GET
+cartsRouter.get("/", async (req, res, next) => {
+    try {
+        const allCarts = await getAllCarts();
+        res.send(allCarts);
+    } catch (error) {
+        next(error);
+    }
+})
+
+//GET cartId
+cartsRouter.get("/cartId", async (req, res, next) => {
+    const { cartId } = req.params;
+
+    try {
+        const cart = await getCartById(cartId);
+        res.send(cart);
+    } catch ({name, message}) {
+        next({name, message})
+    }
+})
 
 // POST//api/ cartId
+cartsRouter.post("/", async (req, res, next) => {
+    try {
+        const { cart } = req.body;
+        const cartObj = { 
+            userId: cart.userId,
+            orderStatus: cart.orderStatus,
+            sessionId: cart.sessionId
+        }
+
+        const createdCart = await createCart(cartObj);
+        if (createdCart) {
+            res.send(createdCart)
+        } else {
+            next({
+                name: 'FailureToCreate',
+                message: 'There was an error starting a new cart'
+            })
+        }
+    } catch ({name, message}) {
+        next({name, message})
+    }
+})
+
+// PATCH cartId
+cartsRouter.patch("/:cartId", async (req, res, next) => {
+    const { cartId } = req.params;
+    const { orderStatus, sessionId } = req.body;
+    const updateFields = {}
+
+    if (orderStatus) {
+        updateFields.orderStatus = orderStatus;
+    }
+
+    if (sessionId) {
+        updateFields.sessionId = sessionId;
+    }
+
+    try {
+        const originalCart = await getCartById(cartId);
+        if (originalCart.userId === req.user.id) {
+            const updatedCart = await updateCart(cartId, updateFields);
+            res.send({cart: updatedCart})
+        } else {
+            next(originalCart ? {
+                name: "UnauthorizedUserError",
+                message: "You can only edit your own cart!"
+            } : {
+                name: "CartNotFoundError",
+                message: "No such cart exists!"
+            })
+        }
+        
+    } catch ({name, message}) {
+        next({name, message})
+    }
+})
 
 // DELETE/ api/ cartid
+cartsRouter.delete("/:cartId", async (req, res, next) => {
+    const { cartId } = req.params;
+    try {
+        const cart = await getCartById(cartId);
+        if (cart && cart.userId === req.user.id) {
+            const deletedCart = await deleteCart(cartId)
+            res.send({ cart: deletedCart });
+        } else {
+            next(cart ? {
+                name: "UnauthorizedUserError",
+                message: "You cannot delete a cart that is not yours!"
+            } : {
+                name: "CartNotFoundError",
+                message: "No such cart exists!"
+            })
+        }
+
+    } catch ({name, message}) {
+        next({name, message})
+    }
+})
 
 module.exports = cartsRouter;
