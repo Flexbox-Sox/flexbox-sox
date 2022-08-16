a269e38c272f441ba6cd38c28b1f309c19ccc090
Sammy_Ester <sester2@outlook.com>
2022-08-16 17:27:46 -0500

    Was able to see the carts on the page for our work

--- src/components/Carts.js
+++ src/components/Carts.js
@@ -1,22 +1,52 @@
 import React from "react";
-import { useHistory,Link } from 'react-router-dom';
+//import { useHistory} from 'react-router-dom';
 // import '../style/Cart.css';
-// import {
-//     getCartById,
-//     createCart,
-//     createCartItem,
-//     attachCartItemtoCarts,
-//     attachProducttoCartItems,
-//     attachCartsToUsers,
-//     updateCart,
-//     deleteCart} from require ('/Final_Capstone/flexbox-sox/db/models/cart');
+const API_URL = 'http://localhost:3000/api'
 
-const Carts=(props)=>{
-const {} = useHistory('');
-const {/*pass down through props: */}= props;
-return(
-    <h1> This is the Carts Page </h1>
-)
+
+
+
+
+const Carts=()=>{
+    // const {} = useHistory('');
+
+    const handlePurchase = async(event)=>{
+
+        await cartItems(event)
+    }
+
+    //Can access information through fetch commands
+    // We need to fetch: the cart information, item information (which carries the db functions) 
+    const cartItems = async () =>{
+        await fetch(`${API_URL}/cartItems/products`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: userData.username,
+                password: userData.password
+            })
+        })
+
+        
+    }
+    return (
+        // I want the products to show based on the product you grab first
+        // need the product information to show in a uniform way (maybe give it an #id for CSS)
+        // **We can make a component to show the total purchase price for the products in cart
+        // once we have the product showing then we will need the button to make the purchase
+        // for each item we will need a handleDelete 
+        // for each item we will need a Qty amount (make it drop down arrow option)
+        // for each item in the cart there is the price amount
+        <div>
+        <h1> This is the carts Page for the items</h1>
+            <div>
+                <ul>Product Items that arranged for purchase</ul>
+                <button onSubmit={handlePurchase}> Proceed to checkout </button>
+            </div>
+        </div>
+    ) 
 
 }
 
